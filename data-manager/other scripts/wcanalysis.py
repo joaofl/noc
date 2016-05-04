ade22412bf8e411253b5c1386243408e5837ebf9
João Loureiro <joaofl@gmail.com>
2016-05-04 01:18:42 +0100

    Model working. Can predict the end to end delay with arbitrary inputs

--- data-manager/other scripts/wcanalysis.py
+++ data-manager/other scripts/wcanalysis.py
@@ -4,77 +4,97 @@
 
 ####### NODE MODEL #######
 #
-#       p_north
-#         ___
-#        |   |
-#   p_west | q |--> p_east
-#        |___|
 #
-#       p_south
+#           ______
+# ____in___|      |
+# ____in___|      |___out____
+# ____in___|      |
+#          |______|
+#
 ##########################
-
+#
 class flow:
-    def __init__(destination=0, r=0, j=0):
-        self.rate = r
-        self.jitter = j
-        self.port_destination = destination
 
-class port:
-    DIR_N = 0
-    DIR_S = 1
-    DIR_E = 2
-    DIR_W = 3
-    DIR_I = 4
+    def __init__(self, period=1, hperiod = 1, jitter=0):
+        self.hperiod = hperiod
+        self.period = period
+        self.jitter = jitter
+        self.released = False
+        self.t = 0
 
-    def __init__(self, direction, in_flow=flow(), out_flow = flow()):
-        self.input = in_flow
-        self.output = out_flow
-        self.direction = direction
-        self.queue = 0
+    #This function will release or not packets according to flows characteristics
+    def generate(self):
+        out = 0
+
+        if self.released == False:
+            if self.t == self.jitter:
+                self.t = 0
+                self.released = True
+                out = 1
+
+        else:
+            if self.t == self.hperiod:
+                self.t = 0;
+
+            if self.t % self.period == 0:
+                out = 1
+
+        self.t += 1
+
+        return out
 
 class node:
-    def __init__(self, id):
+    def __init__(self, id, input_flows=[], output_flow=flow()):
 
-        self.port_north      = port(port.DIR_N)
-        self.port_south      = port(port.DIR_S)
-        self.port_east       = port(port.DIR_E)
-        self.port_west       = port(port.DIR_W)
-        self.port_internal   = port(port.DIR_I)
-        self.ports = [self.port_north, self.port_south, self.port_east, self.port_west, self.port_internal]
+        self.input_flows = input_flows
+        self.output_flow_capacity = output_flow
 
         self.id = id
         self.queue = 0 #Queue initial size
 
         #Packets totals
+        self.stats_tot_transmitted = 0
+        self.stats_tot_received = 0
         self.stats_transmitted = 0
         self.stats_received = 0
+        self.t = -1
 
     def iterate(self):
-        balance_east = 0
+        self.t += 1
 
-        for p in self.ports:
-            if p.input.port_dest == port.DIR_E:
-                balance_east += p.input.rate
-            if p.output.port_dest == port.DIR_E:
-                balance_east += 1
+        input_balance = 0
+        output_balance = 0
+
+        for f in self.input_flows:
+            input_balance += f.generate()
+
+        self.stats_tot_received += input_balance
+        self.stats_received = input_balance
 
 
+        self.queue += input_balance
 
+        output_capacity = self.output_flow_capacity.generate()
+
+        if output_capacity > 0 and self.queue > 0:
+            output_balance = output_capacity
+            self.queue -= output_capacity #queue decreases if no packet comes in and there is outpuc capacity
+            self.stats_tot_transmitted += output_balance
+            self.stats_transmitted = output_balance
+
+        return self.stats_transmitted
 
     def print(self):
 
         print(
-            'n=' + str(self.index) + '\t\t' +
-            'Received:\t' + str(self.stats_received) + '\t' +
-            'Transmitted:\t' + str(self.stats_transmitted) + '\t' +
-            #print('Output rate: ' + str(self.p_east))
-            'Queue size:\t' + str(self.queue)
+            't=' + str(self.t) + '\t\t' +
+            'id=' + str(self.id) + '\t' +
+            'rt:\t' + str(self.stats_tot_received) + '\t' +
+            'tt:\t' + str(self.stats_tot_transmitted) + '\t' +
+            'r:\t' + str(self.stats_received) + '\t' +
+            't:\t' + str(self.stats_transmitted) + '\t' +
+            'q:\t' + str(self.queue)
         )
-
-    def get_queue_size(self):
-        return self.queue
-
-
 
 
 ############# Declaring ###################################
@@ -82,32 +102,49 @@ class node:
 
 
 #tf = 10 #Transmission time slots
-f = flow()
+# out = ''
+# for i in range(0,50):
+#     out += str(f.generate())
+# print(out)
+# exit(0)
+
+
+
+# ia, ib, ic = [flow(period=1, hperiod=1, jitter=1) for i in range(3)]
+ia = flow(period=1, hperiod=1, jitter=0)
+ib = flow(period=1, hperiod=1, jitter=0)
+ic = flow(period=1, hperiod=1, jitter=0)
+id = flow(period=1, hperiod=1, jitter=0) #The flow generated inside has jitter 0 because it is buffered 1 cycle before
+                                        # the neighbors packets arrive
+o = flow(period=1, hperiod=1, jitter=0)
+
 n_nodes = 4
 nodes = []
 
 for i in range(n_nodes):
-    n = node(i) #known constant input rates
+    n = node(i, input_flows=[ia,ib,ic,id], output_flow=o)
     nodes.append(n)
 
+# n = nodes[0]
+# for t in range(10):
+#     n.iterate()
+#     n.print()
+
 ############## Running ####################################
-for n in nodes: #step once inside each
+t = 0
+for n in nodes:
+    for i in range(t+1):
+        n.iterate()
+    t += n.queue + 1
 
-    for i in range(t):
-        n.iterate(p_north=p_north, p_west=p_west, p_south=p_south, p_switch=p_switch)
-        t_next = n.get_queue_size()
-
-    t += t_next
-
-
+wc = nodes[-1].t + 1 #+1 is the the last transmission delay
 
 ############# Showing results ############################
 
 #print('Duration: ' + str(tf) + '\n')
 
-wc = 0
+# wc = 0
 for n in nodes:
     n.print()
-    wc += n.get_queue_size() + 1
 
 print('\nWC=' + str(wc))
